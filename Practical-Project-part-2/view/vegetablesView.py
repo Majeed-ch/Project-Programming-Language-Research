b7406abee38291c3c595d0a0d39f415d319b9b7b
AbdulMajeed <chowdhury4554@gmail.com>
2023-06-03 23:16:24 -0400

    initial project w/ view

---
+++ Practical-Project-part-2/view/vegetablesView.py
@@ -0,0 +1,13 @@
+class VegetablesView:
+    @staticmethod
+    def display_menu():
+        print(
+            "Please select an option:\n\n"
+            + "(1) View all vegetables\n"
+            + "(2) View one vegetable\n"
+            + "(3) Add vegetable\n"
+            + "(4) Update vegetable\n"
+            + "(5) delete vegetable\n"
+            + "(6) Exit"
+        )
+
